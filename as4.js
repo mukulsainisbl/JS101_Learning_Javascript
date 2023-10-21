412ed1e95565647c095f5f358ae98e2b1d5a6f86
mukulsainisbl <mukulsainisbl1234@gmail.com>
2023-10-21 17:40:21 +0530

    Mathematical Operator IWProblems

---
+++ as4.js
@@ -0,0 +1,13 @@
+let a = 15
+let b = 20
+let c = 25
+if (a > b && a > c) {
+  console.log(" A is bigger")
+}
+else if (b > a && b > c) {
+  console.log("B is bigger")
+}
+else (c > a && c > b) {
+
+  console.log("C is bigger")
+}
